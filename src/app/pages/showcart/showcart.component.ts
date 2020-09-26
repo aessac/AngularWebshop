13366e7d945ef6002f6b44b5840b2d0d43e3188e
luxLX2 <luxlxdev@gmail.com>
2020-09-26 15:37:05 +0200

    Clean-up.

--- src/app/pages/showcart/showcart.component.ts
+++ src/app/pages/showcart/showcart.component.ts
@@ -1,6 +1,6 @@
 import { IMovie } from './../../interfaces/IMovie';
 import { CartService } from '../../services/cart/cart.service';
-import { Component, OnInit, ɵConsole } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { faCreditCard, faDollarSign, faExclamationCircle, faShoppingCart, faTrash } from '@fortawesome/free-solid-svg-icons';
 
